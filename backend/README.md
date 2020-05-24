c7ed5c949683a755cb2fe842a7abea4b6c1b45c4
Carlos Roberto <carlosroberto15755@gmail.com>
2020-05-23 22:42:53 -0300

    Backend da aplicacao

---
+++ backend/README.md
@@ -0,0 +1,18 @@
+Desafio Conceitos do Node.js
+
+🚀 Sobre o desafio:
+Deve criar uma aplicação para armazenar repositórios do seu portfólio, 
+que irá permitir a criação, listagem, atualização e remoção dos repositórios, 
+além disso permitir que os repositórios possam receber "likes"
+
+A aplicação teve 5 rotas criar o repositorio, listar, atualizar e remover
+e a rota do like
+
+foram usados os tipos de parametros
+route params
+request body
+
+com a biblioteca uuidv4 importamos a função uuid que é a função que vai criar o
+universally unique identifier, Um identificador único universal 
+e a função isUuid que verifica se um determinado valor é um Uuid
+
---
+++ backend/src/app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid, isUuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories)
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
+
+  repositories.push(repository)
+
+  return response.json(repository)
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if(repositoryIndex < 0 ) {
+    return response.status(400).json({ error: 'repository does not exist'})
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  }
+
+  repositories[repositoryIndex] = repository
+
+  return response.status(200).json(repository)
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if(repositoryIndex < 0 ) {
+    return response.status(400).json({ error: 'repository does not exist'})
+  }
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send()
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if(repositoryIndex < 0 ) {
+    return response.status(400).json({ error: 'repository does not exist'})
+  }
+
+  repositories[repositoryIndex].likes++
+
+  return response.json(repositories[repositoryIndex])
+});
+
+module.exports = app;
